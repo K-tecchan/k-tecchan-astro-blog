f653cee3abee1c5097479f7ec6f89f79cbac5885
k-tecchan <tecchanboy+pc@gmail.com>
2025-09-14 16:25:42 +0900

    astro5にアップデート

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const postCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/post" }),
   schema: z.object({
     title: z.string(),
     description: z.string().max(100, "descriptionは100文字以内"),
